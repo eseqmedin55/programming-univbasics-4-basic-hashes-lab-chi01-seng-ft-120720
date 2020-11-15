b264816963785d08d21a1ea29fdbb62203ca650c
esequiel medina <medinaesequiel@gmail.com>
2020-11-15 17:09:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,9 @@ def my_hash
   end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {:name => "grass hopper"}
 end
+ 
 
 
 def id_hash_generator(number)
